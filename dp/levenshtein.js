c24ceedbca9d051258da528d27410b85780c2ae6
Jonah Schwartz <jonahaschwartz@gmail.com>
2015-05-01 23:51:03 -0400

    working on levenshtein/auto correct

--- dp/levenshtein.js
+++ dp/levenshtein.js
@@ -61,6 +61,19 @@ function levenshtein (s_str, t_str) {
 		}
 	}
 
+	// for (var i = 0; i < s_str.length; i++){
+
+	// }
+
+	// for (var j = 0; j < t_str.length; j++){
+	// 	jsonArr.push({
+	// 		"i" : ,
+	// 		"j" : j,
+	// 		"d" : d[i][j]
+	// 	});
+	// }
+
+
 	//console.log("JSON: ", jsonArr);
 
 	//return d[s_len-1][t_len-1], jsonArr;
@@ -68,5 +81,73 @@ function levenshtein (s_str, t_str) {
 	return jsonArr;
 }
 
-var json = levenshtein("saturday","sunday");
-console.log("Levenshtein Distance =", json);
+function levenBoard (svgContainer, jsonArr, s_str, t_str) {
+	
+	var s = s_str.split("");
+	var t = t_str.split("");
+
+	//Adding rectangle objects to SVG
+	var squares = svgContainer.selectAll("rect")
+		.data(jsonArr)
+		.enter()
+		.append("rect");
+
+	//Drawing rectangles
+	var squareAttributes = squares
+		.attr("x", function (d) {return 50*d.i + 10;})
+		.attr("y", function (d) {return 50*d.j + 10;})
+		.attr("width", 50)
+		.attr("height", 50)
+		.style("stroke", "black")
+		.style("stroke-width", 2)
+		.attr("fill", "white");
+
+	//Adding text objects to SVG
+	var text = svgContainer.selectAll("text")
+		.data(jsonArr)
+		.enter()
+		.append("text");
+
+	//Drawing text
+	var textLabels = text
+		.attr("x", function (d) {return 50*d.i + 35;})
+		.attr("y", function (d) {return 50*d.j + 35;})
+		.text(function (d) {return d.d;})
+		.attr("font-family", "sans-serif")
+		.attr("font-size", "14px")
+		.attr("fill", "black");
+
+	var jsonLabels = [
+		{x: 85, y: 50, text: s[0]},
+		{x: 85, y: 50, text: s[1]},
+		{x: 85, y: 50, text: s[2]},
+		{x: 85, y: 50, text: s[3]},
+		{x: 85, y: 50, text: s[4]},
+		{x: 85, y: 50, text: s[5]},
+		{x: 85, y: 50, text: s[6]}
+	];
+
+	// for (i = 0; i < s.length; i++){
+	// 	jsonLabels.push({
+	// 		x: 85,
+	// 		y: 50,
+	// 		text: s[i]
+	// 	});
+	// 	console.log(s[i]);
+	// }
+
+	var text = svgContainer.selectAll("text")
+		.data(jsonLabels)
+		.enter()
+		.append("text");
+
+	//Drawing text
+	var textLabels = text
+		.attr("x", function (d) {return d.x;})
+		.attr("y", function (d) {return d.y;})
+		.text(function (d) {return d.text;})
+		.attr("font-family", "sans-serif")
+		.attr("font-size", "14px")
+		.attr("fill", "black");
+
+}
